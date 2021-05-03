d85a24a4e5e54e8b0be29930f4fb051ffa91743c
Dilara <dilaraaozk@gmail.com>
2021-05-03 18:28:03 +0300

    first commit.

---
+++ staticDemo/src/staticDemo/DatabaseHelper.java
@@ -0,0 +1,19 @@
+package staticDemo;
+
+public class DatabaseHelper {
+	public static class Crud { // Create Read Update Delete //bu bir inner class örneði çok önerilmiyor.
+		public static void Delete() {
+
+		}
+
+		public static void Update() {
+
+		}
+	}
+	
+	public static class Connection {
+		public static void createConnection() {
+			
+		}
+	}
+}
